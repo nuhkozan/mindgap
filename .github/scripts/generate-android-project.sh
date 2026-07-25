4f2cd02a32d8d168ed6de072ded0c74c74787041
nuhkozan <nuh.kozan118@gmail.com>
2026-07-25 21:25:05 +0300

    Add files via upload

--- .github/scripts/generate-android-project.sh
+++ .github/scripts/generate-android-project.sh
@@ -210,6 +210,8 @@ package com.mindgap.app;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -260,6 +262,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        hideSystemBars();
 
         webView = new WebView(this);
         setContentView(webView);
@@ -415,6 +422,23 @@ public class MainActivity extends Activity {
                     consentInformation.getPrivacyOptionsRequirementStatus()
                             == ConsentInformation.PrivacyOptionsRequirementStatus.REQUIRED;
         }
+    }
+
+    private void hideSystemBars() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemBars();
     }
 
     @Override
