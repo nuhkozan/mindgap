7f26fcccb735862e2fd3f6334aa4086b122c45d6
nuhkozan <nuh.kozan118@gmail.com>
2026-08-29 12:50:31 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mindgap-v7'; // ⚠️ Her yeni index.html güncellemesinde bu numarayı artır (v8, v9...)
+const CACHE_NAME = 'mindgap-v8'; // ⚠️ Her yeni index.html güncellemesinde bu numarayı artır (v9, v10...)
 const ASSETS = [
   './index.html',
   './manifest.json',
